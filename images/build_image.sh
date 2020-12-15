b1b39c7e617fd532d4d4faf29be7cf50a39430d7
Ferenc Géczi <geczi@univention.de>
2020-12-15 16:52:55 +0100

    feat: Add container-entrypoint.sh
    * The container-entrypoint.sh generates slapd.conf during startup.
    * The resolution of the templating variables in the slapd.conf fragments
      is performed by solve.py which is a substitute ucr filter.
    * The dummy ucr is there to break the recursion when a templated
      slapd.conf fragment opens a subprocess with "ucr filter".

--- images/build_image.sh
+++ images/build_image.sh
@@ -5,18 +5,46 @@ SUITE=${SUITES[1]}
 OMAR_UCS_5="/var/univention/buildsystem2/apt/ucs_5.0-0/"
 UNIV_PATCHED_SLAPD_DEB="amd64/slapd_2.4.47+dfsg-3+deb10u2A~5.0.0.202008031848_amd64.deb"
 UNIV_PATCHED_LLDAP_DEB="amd64/libldap-2.4-2_2.4.47+dfsg-3+deb10u2A~5.0.0.202008031848_amd64.deb"
-#UNIV_LDAP_SRV="all/univention-ldap-server_16.0.4-2A~5.0.0.202011301749_all.deb"
+UNIV_LDAP_SRV="all/univention-ldap-server_16.0.4-2A~5.0.0.202011301749_all.deb"
+UNIV_SAML_SCH="./all/univention-saml-schema_7.0.4-2A~5.0.0.202012081116_all.deb"
+UNIV_VIRT_SCH="./all/univention-virtual-machine-manager-schema_10.0.0-1A~5.0.0.202006052312_all.deb"
+UNIV_MMBR_SCH="./all/univention-ldap-overlay-memberof_12.0.0-1A~5.0.0.202009231618_all.deb"
+UNIV_ACLM_SCH="./all/univention-ldap-acl-master_16.0.4-2A~5.0.0.202011301749_all.deb"
+UNIV_APPC_SCH="./all/univention-appcenter_9.0.1-7A~5.0.0.202012141225_all.deb"
+
 #UNIV_NOTIFIER="amd64/univention-directory-notifier_14.0.4-3A~5.0.0.202012011441_amd64.deb"
 if [[ ! -f "${UNIV_PATCHED_SLAPD_DEB##*/}" ]];
 then
     scp "omar:${OMAR_UCS_5}/${UNIV_PATCHED_SLAPD_DEB}" .
     scp "omar:${OMAR_UCS_5}/${UNIV_PATCHED_LLDAP_DEB}" .
-    #scp "omar:${OMAR_UCS_5}/${UNIV_LDAP_SRV}" .
+    scp "omar:${OMAR_UCS_5}/${UNIV_LDAP_SRV}" .
+    scp "omar:${OMAR_UCS_5}/${UNIV_SAML_SCH}" .
+
     #scp "omar:${OMAR_UCS_5}/${UNIV_NOTIFIER}" .
     ar vx "${UNIV_PATCHED_SLAPD_DEB##*/}" data.tar.xz
     tar -xf data.tar.xz -C univention-openldap/
     ar vx "${UNIV_PATCHED_LLDAP_DEB##*/}" data.tar.xz
     tar -xf data.tar.xz -C univention-openldap/
+    ar vx "${UNIV_LDAP_SRV##*/}" data.tar.xz
+    tar -xf data.tar.xz -C univention-openldap/
+
+    ar vx "${UNIV_SAML_SCH##*/}" data.tar.xz
+    tar -C univention-openldap/ -xf data.tar.xz ./etc/univention/templates/files/etc/ldap/slapd.conf.d/
+
+    ar vx "${UNIV_VIRT_SCH##*/}" data.tar.xz
+    tar -C univention-openldap/ -xf data.tar.xz ./etc/univention/templates/files/etc/ldap/slapd.conf.d/
+
+    ar vx "${UNIV_MMBR_SCH##*/}" data.tar.xz
+    # TODO: Do we need the univention-update-memberof python tool?
+    tar -C univention-openldap/ -xf data.tar.xz ./etc/univention/templates/files/etc/ldap/slapd.conf.d/
+
+    ar vx "${UNIV_ACLM_SCH##*/}" data.tar.xz
+    tar -C univention-openldap/ -xf data.tar.xz ./etc/univention/templates/files/etc/ldap/slapd.conf.d/
+
+    ar vx "${UNIV_APPC_SCH##*/}" data.tar.xz
+    tar -C univention-openldap/ -xf data.tar.xz ./usr/share/univention-appcenter/66univention-appcenter_app.acl
+    mv univention-openldap/usr/share/univention-appcenter/66univention-appcenter_app.acl \
+       univention-openldap/etc/univention/templates/files/etc/ldap/slapd.conf.d/
 
     # TODO: Use /usr/share/univention-ssl/make-certificates.sh
     scp galant:~fgeczi/ssl_fg-organization.tar.bz2 .
@@ -37,6 +65,8 @@ then
     # TODO: Find a way to have the translog overlay do this
     mkdir -p univention-openldap/var/lib/univention-ldap/listener/
     touch univention-openldap/var/lib/univention-ldap/listener/listener
+
+    cp ucr solve.py univention-openldap/usr/sbin/
 fi
 
 docker build -t univention-openldap:${SUITE}-slim-portable \
---
+++ images/univention-openldap/container-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cat /etc/univention/templates/files/etc/ldap/slapd.conf.d/* | solve.py --ldapbase 'dc=fg-organization,dc=intranet' --domainname 'fg-organization.intranet' > /etc/ldap/slapd.conf
+
+# TODO: Remove this
+sed -i '/^rootdn\t\t.*/a rootpw\t\t"univention"' /etc/ldap/slapd.conf
+
+
+
+/usr/sbin/slapd -f /etc/ldap/slapd.conf -d 9999 -h "ldapi:/// ldap://:389/ ldaps://:636/"
