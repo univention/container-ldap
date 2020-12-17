c76f538500ab395128a87ae75aa8d0a9e24bba13
Ferenc Géczi <geczi@univention.de>
2020-12-17 17:14:12 +0100

    feat: Generate base ldif in runtime

--- images/build_image.sh
+++ images/build_image.sh
@@ -5,12 +5,13 @@ SUITE=${SUITES[1]}
 OMAR_UCS_5="/var/univention/buildsystem2/apt/ucs_5.0-0/"
 UNIV_PATCHED_SLAPD_DEB="amd64/slapd_2.4.47+dfsg-3+deb10u2A~5.0.0.202008031848_amd64.deb"
 UNIV_PATCHED_LLDAP_DEB="amd64/libldap-2.4-2_2.4.47+dfsg-3+deb10u2A~5.0.0.202008031848_amd64.deb"
+UNIV_LDAP_CFG="all/univention-ldap-config_16.0.4-2A~5.0.0.202011301749_all.deb"
 UNIV_LDAP_SRV="all/univention-ldap-server_16.0.4-2A~5.0.0.202011301749_all.deb"
-UNIV_SAML_SCH="./all/univention-saml-schema_7.0.4-2A~5.0.0.202012081116_all.deb"
-UNIV_VIRT_SCH="./all/univention-virtual-machine-manager-schema_10.0.0-1A~5.0.0.202006052312_all.deb"
-UNIV_MMBR_SCH="./all/univention-ldap-overlay-memberof_12.0.0-1A~5.0.0.202009231618_all.deb"
-UNIV_ACLM_SCH="./all/univention-ldap-acl-master_16.0.4-2A~5.0.0.202011301749_all.deb"
-UNIV_APPC_SCH="./all/univention-appcenter_9.0.1-7A~5.0.0.202012141225_all.deb"
+UNIV_SAML_SCH="all/univention-saml-schema_7.0.4-2A~5.0.0.202012081116_all.deb"
+UNIV_VIRT_SCH="all/univention-virtual-machine-manager-schema_10.0.0-1A~5.0.0.202006052312_all.deb"
+UNIV_MMBR_SCH="all/univention-ldap-overlay-memberof_12.0.0-1A~5.0.0.202009231618_all.deb"
+UNIV_ACLM_SCH="all/univention-ldap-acl-master_16.0.4-2A~5.0.0.202011301749_all.deb"
+UNIV_APPC_SCH="all/univention-appcenter_9.0.1-7A~5.0.0.202012141225_all.deb"
 
 #UNIV_NOTIFIER="amd64/univention-directory-notifier_14.0.4-3A~5.0.0.202012011441_amd64.deb"
 if [[ ! -f "${UNIV_PATCHED_SLAPD_DEB##*/}" ]];
@@ -27,6 +28,12 @@ then
     tar -xf data.tar.xz -C univention-openldap/
     ar vx "${UNIV_LDAP_SRV##*/}" data.tar.xz
     tar -xf data.tar.xz -C univention-openldap/
+
+    ar vx "${UNIV_LDAP_CFG##*/}" data.tar.xz
+    tar -C univention-openldap/ -xf data.tar.xz ./usr/share/univention-ldap/base.ldif \
+                                                ./usr/share/univention-ldap/core-edition.ldif \
+                                                ./usr/share/univention-ldap/translog.ldif \
+                                                ./usr/share/univention-ldap/ffpu.ldif
 
     ar vx "${UNIV_SAML_SCH##*/}" data.tar.xz
     tar -C univention-openldap/ -xf data.tar.xz ./etc/univention/templates/files/etc/ldap/slapd.conf.d/
@@ -53,9 +60,6 @@ then
     # TODO: Extract schemas from NOTIFIER and LDAP_SRV
     scp galant:~fgeczi/univention-ldap_schemas.tar.bz2 .
     tar -xf univention-ldap_schemas.tar.bz2 -C univention-openldap/
-
-    # TODO: Eliminate this as soon as it can be generated
-    cp slapd.conf_current univention-openldap/etc/ldap/slapd.conf
 
     # As seen in /usr/share/univention-ldap/create-dh-parameter-files
     openssl dhparam -out "dh_2048.pem" -2 2048
--- images/univention-openldap/container-entrypoint.sh
+++ images/univention-openldap/container-entrypoint.sh
@@ -1,12 +1,58 @@
 #!/usr/bin/env bash
 
+set -x
+
+setup_slapd_conf() {
+
+    cat /etc/univention/templates/files/etc/ldap/slapd.conf.d/* |\
+    solve.py --ldapbase 'dc=fg-organization,dc=intranet' \
+             --domainname 'fg-organization.intranet' \
+             > /etc/ldap/slapd.conf
+}
+
+setup_initial_ldif() {
+    # Inspired by 01univention-ldap-server-init.inst
+    pw_crypt="univention"
+    backup_crypt="univention"
+    ldap_base="dc=fg-organization,dc=intranet"
+    domainname="fg-organization.intranet"
+    sambadomain="${domainname%%.*}"
+    realm="$(echo "$domainname" | sed -e 's/dc=//g;s/,/./g;s/[a-z]/\u&/g')"
+    firstdc="$(echo "$ldap_base" | sed -e 's|,.*||g;s|.*=||')"
+
+    # TODO: use check /usr/sbin/univention-newsid
+    sid="S-1-5-21-4181270633-4020214626-836608356"
+
+    printf -v filter_string '%s' \
+         "s|@@%%@@ldap\.pw@@%%@@|$pw_crypt|;"\
+         "s|@@%%@@backup\.pw@@%%@@|$backup_crypt|;"\
+         "s|@@%%@@sambadomain@@%%@@|$sambadomain|;"\
+         "s|@@%@@sambadomain@@%@@|$sambadomain|;"\
+         "s|@@%%@@firstdc@@%%@@|$firstdc|;"\
+         "s|@@%%@@realm@@%%@@|$realm|;"\
+         "s|@@%%@@sid@@%%@@|$sid|;"\
+         "s|@@%@@domain@@%@@|$domainname|"
+
+    cat /usr/share/univention-ldap/base.ldif \
+        /usr/share/univention-ldap/core-edition.ldif |\
+    solve.py --ldapbase "${ldap_base}" --domainname "${domainname}"|\
+    sed -e "${filter_string}" | slapadd -f /etc/ldap/slapd.conf 
+}
+
+setup_translog_ldif() {
+    # Inspired by /usr/share/univention-ldap/setup-translog
+    slapadd -f /etc/ldap/slapd.conf -b cn=translog \
+            -l /usr/share/univention-ldap/translog.ldif
+}
+
+
 touch /var/lib/univention-ldap/listener/listener
 
-cat /etc/univention/templates/files/etc/ldap/slapd.conf.d/* | solve.py --ldapbase 'dc=fg-organization,dc=intranet' --domainname 'fg-organization.intranet' > /etc/ldap/slapd.conf
+setup_slapd_conf
+setup_initial_ldif
+setup_translog_ldif
 
 # TODO: Remove this
 sed -i '/^rootdn\t\t.*/a rootpw\t\t"univention"' /etc/ldap/slapd.conf
-
-
 
 /usr/sbin/slapd -f /etc/ldap/slapd.conf -d 9999 -h "ldapi:/// ldap://:389/ ldaps://:636/"
