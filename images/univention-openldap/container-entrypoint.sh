9c333e377db5c13a20f6472eb041054ea9992e21
Ferenc Géczi <geczi@univention.de>
2020-12-21 10:48:12 +0100

    feat: Persist last_id onto last-id-data volume

--- images/univention-openldap/container-entrypoint.sh
+++ images/univention-openldap/container-entrypoint.sh
@@ -7,6 +7,27 @@ init_variables() {
     LDAP_BASE_DN="${LDAP_BASE_DN:dc=fg-organization,dc=intranet}"# dc=univention-organization,dc=intranet
 }
 
+setup_listener_path() {
+    # TODO: Shouldn't the translog overlay be able to do this?
+    mkdir -p /var/lib/univention-ldap/listener/
+    touch /var/lib/univention-ldap/listener/listener
+}
+
+setup_last_id_path() {
+    # Docker persistent named volumes can only be mapped to directories
+    # and since certain directories under /var/lib/univention-ldap/
+    # have to be taken from the image we can't map a volume to
+    # /var/lib/univention-ldap/ directly.
+    # The last_id path is hardcoded in translog overlay and can't be changed.
+    # So in order to keep the last_id file on a separate volum
+    # we mount a volume under /var/lib/univention-ldap/last-id-data/
+    # and create a symbolic link in runtime.
+    mkdir -p /var/lib/univention-ldap/last-id-data/
+    touch /var/lib/univention-ldap/last-id-data/last_id
+    ln -s /var/lib/univention-ldap/last-id-data/last_id \
+          /var/lib/univention-ldap/last_id
+}
+
 setup_slapd_conf() {
 
     cat /etc/univention/templates/files/etc/ldap/slapd.conf.d/* |\
@@ -25,7 +46,7 @@ setup_initial_ldif() {
     realm="$(echo "$domainname" | sed -e 's/dc=//g;s/,/./g;s/[a-z]/\u&/g')"
     firstdc="$(echo "$ldap_base" | sed -e 's|,.*||g;s|.*=||')"
 
-    # TODO: use check /usr/sbin/univention-newsid
+    # TODO: check /usr/sbin/univention-newsid
     sid="S-1-5-21-4181270633-4020214626-836608356"
 
     printf -v filter_string '%s' \
@@ -50,12 +71,6 @@ setup_translog_ldif() {
             -l /usr/share/univention-ldap/translog.ldif
 }
 
-setup_listener_path() {
-    # TODO: Shouldn't the translog overlay be able to do this?
-    mkdir -p /var/lib/univention-ldap/listener/
-    touch /var/lib/univention-ldap/listener/listener
-}
-
 setup_administrator_user() {
     printf -v password_hash '%s' \
             '$6$6M7LMsXo2wgniZGE$tGxma/MBb1kUqx9.GZ8UwpvEwOXUXal' \
@@ -78,10 +93,11 @@ setup_ssl_certificates() {
 
 
 init_variables
+setup_listener_path
+setup_last_id_path
 setup_slapd_conf
 setup_initial_ldif
 setup_translog_ldif
-setup_listener_path
 setup_administrator_user
 setup_ssl_certificates
 
