1a56d511b24a28b99966e768f27bd1655d40bd05
Per Macha <macha@univention.de>
2021-07-22 00:00:00 +0200

    feat(ci): push to quay [iam-issues#150]

--- ci-build-script.py
+++ ci-build-script.py
@@ -7,7 +7,6 @@
 # included
 import os
 import sys
-import time
 
 # third party
 import sh  # pylint: disable=import-error
@@ -25,108 +24,44 @@ import ci_vars  # noqa: E402; pylint: disable=import-error,wrong-import-position
 import ci_version  # noqa: E402,E501; pylint: disable=import-error,wrong-import-position
 
 # pylint: disable=not-callable
-sh_noout = sh(_cwd=BASE_DIR)
-
-# pylint: disable=not-callable
 sh_out = sh(_out='/dev/stdout', _err='/dev/stderr', _cwd=BASE_DIR)
 
 
 def main(service):  # pylint: disable=too-many-locals
     """The main script builds, labels and pushes"""
 
-    docker_env = ci_vars.get_env_vars(ci_vars.MINIMAL_DOCKER_VARS)
-
-    pull_push_env = ci_vars.get_env_vars(ci_vars.ADDITIONAL_PULL_PUSH_VARS)
-    pull_push_env.update(docker_env)
-
-    compose_env = {
-        'COMPOSE_DOCKER_CLI_BUILD': '0',
-        'CI_PROJECT_URL': 'unset',
-        'CI_PIPELINE_ID': ci_vars.DEFAULT_CI_PIPELINE_ID,
-        'LANG': 'C.UTF-8',
-    }
-
-    if 'CI_JOB_STARTED_AT' not in os.environ:
-        compose_env['CI_JOB_STARTED_AT'] = time.strftime(
-            "%Y-%m-%dT%H:%M:%SZ", time.gmtime()
-        )
-
-    if 'CI_COMMIT_SHA' not in os.environ:
-        # pylint: disable=too-many-function-args
-        compose_env['CI_COMMIT_SHA'] = (
-            sh_noout.git('rev-parse', 'HEAD').stdout.rstrip().decode('ascii')
-        )
-
-    compose_env.update(ci_vars.get_env_vars(ci_vars.ADDITIONAL_COMPOSE_VARS))
-    compose_env.update(docker_env)
-
-    docker_compose_build_files = os.environ.get(
-        'DOCKER_COMPOSE_BUILD_FILES',
-        ci_vars.DEFAULT_DOCKER_COMPOSE_BUILD_FILES,
-    )
-
-    ci_pipeline_id = compose_env['CI_PIPELINE_ID']
-
-    upx_image_registry = os.environ.get(
-        'UPX_IMAGE_REGISTRY',
-        ci_vars.DEFAULT_UPX_IMAGE_REGISTRY,
-    )
+    envs = ci_vars.get_docker_envs(BASE_DIR, pull_push=True, compose=True)
 
     sh_out.cp('.env.ldap-server.example', '.env.ldap-server')
+
     sh_out.docker_compose(
-        docker_compose_build_files.split(),
+        envs['common']['DOCKER_COMPOSE_BUILD_FILES'].split(),
         'build',
         service if service else '--parallel',
-        _env=compose_env,
+        _env=envs['compose'],
     )
 
     services = ('notifier', 'server')
     if service:
         services = (service, )
     for cur_service in services:
-        image_path = '{}ldap/{}'.format(upx_image_registry, cur_service)
+        image_name = 'upx-ldap-{}'.format(cur_service)
 
         try:
+            # push tags "build-<ci-pipeline-id>" and
+            #   "<version>-<ci-pipeline-id>"
             ci_docker.add_and_push_build_version_label_and_tag(
-                image_path,
-                ci_pipeline_id,
-                docker_env,
-                pull_push_env,
+                image_name,
+                envs['common']['CI_PIPELINE_ID'],
+                envs['docker'],
+                envs['pull_push'],
             )
         except ci_version.AppVersionNotFound:
             log.error('app version not found')
             return 2
-
-        if cur_service == 'server':
-            quay_image_registry = os.environ.get(
-                'QUAY_IMAGE_REGISTRY',
-                ci_vars.DEFAULT_QUAY_IMAGE_REGISTRY,
-            )
-            quay_image_name = 'univention-upx-container-ldap-openldap'
-            quay_server_path = '{}{}'.format(
-                quay_image_registry, quay_image_name
-            )
-            build_path = '{}:build-{}'.format(image_path, ci_pipeline_id)
-            app_version = ci_version.get_app_version(build_path, docker_env)
-            clean_version = ci_version.cleanup_for_docker_tag(app_version)
-            tag = '{}:{}-{}'.format(
-                quay_server_path, clean_version, ci_pipeline_id
-            )
-            ci_docker.add_and_push_tag(
-                build_path, tag, docker_env, pull_push_env
-            )
-
-    # push tag "build-<ci-pipeline-id>"
-    args = [
-        docker_compose_build_files.split(),
-        'push',
-    ]
-    if service:
-        args.append(service)
-    sh_out.docker_compose(
-        *args,
-        _env=compose_env,
-    )
+        except ci_docker.DockerPushFailed:
+            log.error('docker push failed')
+            return 3
 
     return 0
 
--- ci-publish-script.py
+++ ci-publish-script.py
@@ -8,9 +8,6 @@
 import os
 import sys
 
-# third party
-import sh  # pylint: disable=import-error
-
 # internal imports
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 LIBS_DIR = os.path.join(BASE_DIR, 'lib')
@@ -23,61 +20,35 @@ from ci_log import (  # noqa: E402,E501; pylint: disable=import-error,wrong-impo
 import ci_vars  # noqa: E402; pylint: disable=import-error,wrong-import-position
 import ci_version  # noqa: E402,E501; pylint: disable=import-error,wrong-import-position
 
-# pylint: disable=not-callable
-sh_out = sh(_out='/dev/stdout', _err='/dev/stderr', _cwd=BASE_DIR)
-
 
 def main(service):
     """The main script builds, labels and pushes"""
 
-    docker_env = ci_vars.get_env_vars(ci_vars.MINIMAL_DOCKER_VARS)
-
-    pull_push_env = ci_vars.get_env_vars(ci_vars.ADDITIONAL_PULL_PUSH_VARS)
-    pull_push_env.update(docker_env)
-
-    ci_pipeline_id = os.environ.get(
-        'CI_PIPELINE_ID', ci_vars.DEFAULT_CI_PIPELINE_ID
-    )
-
-    upx_image_registry = os.environ.get(
-        'UPX_IMAGE_REGISTRY',
-        ci_vars.DEFAULT_UPX_IMAGE_REGISTRY,
-    )
+    envs = ci_vars.get_docker_envs(BASE_DIR, pull_push=True)
 
     services = ('notifier', 'server')
     if service:
         services = (service, )
     for cur_service in services:
-        image_path = '{}ldap/{}'.format(upx_image_registry, cur_service)
-        push_paths = [image_path]
-        if cur_service == 'server':
-            quay_image_registry = os.environ.get(
-                'QUAY_IMAGE_REGISTRY',
-                ci_vars.DEFAULT_QUAY_IMAGE_REGISTRY,
+        image_name = 'upx-ldap-{}'.format(cur_service)
+
+        try:
+            # push tags "latest" and "<version>"
+            ci_docker.pull_add_push_publish_version_tag(
+                image_name,
+                envs['common']['CI_PIPELINE_ID'],
+                envs['docker'],
+                envs['pull_push'],
             )
-            quay_image_name = 'univention-upx-container-ldap-openldap'
-            quay_server_path = '{}{}'.format(
-                quay_image_registry, quay_image_name
-            )
-            push_paths.append(quay_server_path)
-        for push_path in push_paths:
-            try:
-                ci_docker.pull_add_push_publish_version_tag(
-                    image_path,
-                    ci_pipeline_id,
-                    docker_env,
-                    pull_push_env,
-                    push_path,
-                )
-            except ci_version.AppVersionNotFound:
-                log.error('app version not found')
-                return 2
-            except ci_docker.DockerPullFailed:
-                log.error('dock pull failed')
-                return 1
-            except ci_docker.DockerPushFailed:
-                log.error('docker push failed')
-                return 3
+        except ci_version.AppVersionNotFound:
+            log.error('app version not found')
+            return 2
+        except ci_docker.DockerPullFailed:
+            log.error('docker pull failed')
+            return 1
+        except ci_docker.DockerPushFailed:
+            log.error('docker push failed')
+            return 3
 
     return 0
 
