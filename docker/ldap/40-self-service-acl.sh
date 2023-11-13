7e42e666b4fab49c33f0b887db3c2c8fc4fe212f
Thomas Kintscher <thomas.kintscher.extern@univention.de>
2023-11-13 19:13:49 +0100

    feat(server): add ACL for self-service

---
+++ docker/ldap/40-self-service-acl.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+##############################################################################
+# Generate the LDAP ACLs for the self-service from UCR variable.
+#
+# In UCS, this runs as a UCR hook observing `self-service/ldap_attributes`.
+# This hook then writes the ACL to LDAP using UDM (`settings/ldapacl`).
+# The Listener plugin will observice this change, generate the ACL snippet,
+# regenerate slapd.conf and restart slapd.
+#
+# This flow is incompatible with container operation.
+#
+# Here we just generate the ACL on container startup and perform the little
+# sanitization that `self-service-acl.py` would do: remove all whitespace
+# around the LDAP attribute names
+#
+##############################################################################
+
+LDAP_ATTRIBUTES=$(
+    echo "@%@self-service/ldap_attributes@%@" \
+      | ucr-light-filter \
+      | sed --expression 's/[[:blank:]]*,[[:blank:]]*/,/g' \
+      || echo ""
+)
+
+if [ -n "${LDAP_ATTRIBUTES:-}" ]; then
+    {
+        echo ""
+        echo "access to filter=\"univentionObjectType=users/user\" attrs=${LDAP_ATTRIBUTES}"
+        echo "    by self write"
+        echo "    by * +0 break"
+        echo ""
+    } > /etc/univention/templates/files/etc/ldap/slapd.conf.d/64selfservice_userattributes.acl
+fi
