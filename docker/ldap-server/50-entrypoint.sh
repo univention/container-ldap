c055448412dfc6006f0326a39ac1561fa858d0c5
Jaime Conde <conde-segovia@univention.de>
2024-12-04 22:25:49 +0100

    fix(ldap-server): LDAP database initialization in mirror-mode

--- docker/ldap-server/50-entrypoint.sh
+++ docker/ldap-server/50-entrypoint.sh
@@ -134,9 +134,11 @@ setup_initial_ldif() {
     database_name="data"
   fi
 
+  # FIXME: Should we just check for files in that folder? There is also lock.mdb
   files="$(find /var/lib/univention-ldap/ldap/ -name "${database_name}.*" -type f)"
 
   if [[ -n "${files}" ]]; then
+    echo "Database already exists, skipping initial LDIF setup"
     return 0
   fi
 
