d38d2a62e49d6ea1ea50b4e77ebb6a599ed3013f
Johannes Lohmer <lohmer@univention.de>
2024-12-05 21:49:34 +0100

    fix(ldap-server): Check LDAP database initialization flag in configmap

--- docker/ldap-server/evaluate_database_init.py
+++ docker/ldap-server/evaluate_database_init.py
@@ -2,84 +2,100 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
+import argparse
 import logging
 import sys
 
 from kubernetes import client, config
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def check_configmap():
+
+# FIXME: Better name if we are not planning to use it in the future.
+# Something like ldap-database-status or similar.
+# ~~Or even make it configurable via an environment variable.~~
+# Configurable is problematic because it introduces migration headaches!
+CONFIGMAP_NAME = "nubus-deployment-status"
+DATABASE_INITIALIZED_KEY = "ldap_database_initialized"
+
+
+def database_needs_initialization(v1: client.CoreV1Api, namespace: str):
     """
     Check if the ConfigMap exists and has the key ldap_database_initialized set to true.
 
     Returns:
-        0: Database needs initialization: ConfigMap does not exist or ldap_database_initialized is set to false.
+        0: Database needs initialization: ldap_database_initialized is set to false.
         1: Database already initialized: ldap_database_initialized is already true in the ConfigMap.
-        2: LDAP server should terminate: Unexpected error accessing the ConfigMap.
+        2: LDAP server should terminate: Unexpected error accessing or parsing the ConfigMap.
     """
 
-    def needs_initialization():
-        logger.info("Database needs initialization.")
-        # FIXME: We could set the ConfigMap key to `true` here, but if the
-        # initialization fails and the LDAP server restarts, the variable will
-        # be set to true on the next evaluation. This would be wrong.
-        sys.exit(0)
+    try:
+        configmap = v1.read_namespaced_config_map(name=CONFIGMAP_NAME, namespace=namespace)
 
-    def already_initialized():
-        logger.info("Database already initialized.")
-        sys.exit(1)
+        if DATABASE_INITIALIZED_KEY not in configmap.data:
+            logger.error("ConfigMap does not contain the key `%s`." % DATABASE_INITIALIZED_KEY)
+            raise ValueError("Invalid ConfigMap structure")
 
-    def terminate(error):
-        logger.error("Unexpected error accessing the ConfigMap.")
+        if configmap.data[DATABASE_INITIALIZED_KEY].lower() == "true":
+            logger.info("Database needs initialization.")
+            sys.exit(0)
+
+    except Exception as error:
+        logger.error("Unexpected error evaluating the database initialization status.")
+        logger.error(error)
+        sys.exit(2)
+
+    logger.info("Database already initialized.")
+    sys.exit(1)
+
+
+def database_initialized(v1: client.CoreV1Api, namespace: str):
+    try:
+        configmap = v1.read_namespaced_config_map(name=CONFIGMAP_NAME, namespace=namespace)
+
+        if DATABASE_INITIALIZED_KEY not in configmap.data:
+            logger.error("ConfigMap does not contain the key `%s`." % DATABASE_INITIALIZED_KEY)
+            raise ValueError("Invalid ConfigMap structure")
+
+        configmap.data[DATABASE_INITIALIZED_KEY] = "true"
+
+        v1.replace_namespaced_config_map(name=CONFIGMAP_NAME, namespace=namespace, body=configmap)
+
+    except Exception as error:
+        logger.error("Unexpected error updating the database initialization status.")
         logger.error(error)
         sys.exit(2)
 
 
-    try:
-        try:
-            config.load_incluster_config()
-        except config.ConfigException:
-            config.load_kube_config()
+def main():
+    # try:
+    #     config.load_incluster_config()
+    # except config.ConfigException:
+    config.load_kube_config()
 
-        v1 = client.CoreV1Api()
+    v1 = client.CoreV1Api()
 
-        with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r") as f:
-            namespace = f.read()
+    with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r") as f:
+        namespace = f.read()
+    logger.debug("Namespace: %s" % namespace)
 
-        # FIXME: Better name if we are not planning to use it in the future.
-        # Something like ldap-database-status or similar.
-        # Or even make it configurable via an environment variable.
-        configmap_name = "nubus-deployment-status"
-        database_initialized_key = "ldap_database_initialized"
+    parser = argparse.ArgumentParser(description="A script with two subcommands.")
+    subparsers = parser.add_subparsers(dest="command", required=True)
 
-        try:
-            configmap = v1.read_namespaced_config_map(
-                name=configmap_name,
-                namespace=namespace
-            )
+    # Subcommand1
+    parser_command1 = subparsers.add_parser("database-needs-initialization", help="Execute Command1")
+    parser_command1.set_defaults(func=database_needs_initialization)
 
-            if database_initialized_key not in configmap.data:
-                logger.info("ConfigMap does not contain the key `%s`." % database_initialized_key)
-                # FIXME: Should we terminate with 2 if the key is not present?
-                # To notify an operator that the ConfigMap is not correctly configured.
-                needs_initialization()
+    # Subcommand2
+    parser_command2 = subparsers.add_parser("database-initialized", help="Execute Command2")
+    parser_command2.set_defaults(func=database_initialized)
 
-            if configmap.data[database_initialized_key].lower() == 'true':
-                already_initialized()
-            else:
-                needs_initialization()
+    args = parser.parse_args()
 
-        except client.exceptions.ApiException as e:
-            if e.status == 404:
-                logger.info("ConfigMap does not exist.")
-                needs_initialization()
-            else:
-                terminate(e)
+    # Call the associated function
+    args.func(v1, namespace)
 
-    except Exception as e:
-        terminate(e)
 
 if __name__ == "__main__":
-    check_configmap()
+    main()
