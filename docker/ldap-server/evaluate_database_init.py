9e56539e511cdd51c973981701184c4e8dca6675
Johannes Lohmer <lohmer@univention.de>
2024-12-06 10:19:25 +0100

    fix(ldap-server): improve LDAP database initialization script with log level and more

--- docker/ldap-server/evaluate_database_init.py
+++ docker/ldap-server/evaluate_database_init.py
@@ -8,14 +8,9 @@ import sys
 
 from kubernetes import client, config
 
-logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-# FIXME: Better name if we are not planning to use it in the future.
-# Something like ldap-database-status or similar.
-# ~~Or even make it configurable via an environment variable.~~
-# Configurable is problematic because it introduces migration headaches!
 CONFIGMAP_NAME = "nubus-deployment-status"
 DATABASE_INITIALIZED_KEY = "ldap_database_initialized"
 
@@ -38,16 +33,16 @@ def database_needs_initialization(v1: client.CoreV1Api, namespace: str):
             raise ValueError("Invalid ConfigMap structure")
 
         if configmap.data[DATABASE_INITIALIZED_KEY].lower() == "true":
-            logger.info("Database needs initialization.")
-            sys.exit(0)
+            logger.info("Database already initialized.")
+            sys.exit(1)
 
     except Exception as error:
         logger.error("Unexpected error evaluating the database initialization status.")
         logger.error(error)
         sys.exit(2)
 
-    logger.info("Database already initialized.")
-    sys.exit(1)
+    logger.info("Database needs initialization.")
+    sys.exit(0)
 
 
 def database_initialized(v1: client.CoreV1Api, namespace: str):
@@ -66,13 +61,32 @@ def database_initialized(v1: client.CoreV1Api, namespace: str):
         logger.error("Unexpected error updating the database initialization status.")
         logger.error(error)
         sys.exit(2)
+    logger.info("Database initialization status set to true in the %s ConfigMap" % CONFIGMAP_NAME)
 
 
 def main():
-    # try:
-    #     config.load_incluster_config()
-    # except config.ConfigException:
-    config.load_kube_config()
+    parser = argparse.ArgumentParser(
+        description="Script to check LDAP database initialization status in Nubus for Kubernetes"
+    )
+    parser.add_argument("--log-level", default="info", help="Set the log level, default is INFO")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+    command_needs_initialization = subparsers.add_parser(
+        "database-needs-initialization",
+        help="Check the LDAP database initialization status, returns 0 if initialization is required.",
+    )
+    command_needs_initialization.set_defaults(func=database_needs_initialization)
+    command_initialized = subparsers.add_parser(
+        "database-initialized", help="Set the LDAP database initialization status to true / initialized"
+    )
+    command_initialized.set_defaults(func=database_initialized)
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level.upper())
+
+    try:
+        config.load_incluster_config()
+    except config.ConfigException:
+        config.load_kube_config()
 
     v1 = client.CoreV1Api()
 
@@ -80,20 +94,6 @@ def main():
         namespace = f.read()
     logger.debug("Namespace: %s" % namespace)
 
-    parser = argparse.ArgumentParser(description="A script with two subcommands.")
-    subparsers = parser.add_subparsers(dest="command", required=True)
-
-    # Subcommand1
-    parser_command1 = subparsers.add_parser("database-needs-initialization", help="Execute Command1")
-    parser_command1.set_defaults(func=database_needs_initialization)
-
-    # Subcommand2
-    parser_command2 = subparsers.add_parser("database-initialized", help="Execute Command2")
-    parser_command2.set_defaults(func=database_initialized)
-
-    args = parser.parse_args()
-
-    # Call the associated function
     args.func(v1, namespace)
 
 
