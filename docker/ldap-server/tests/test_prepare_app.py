42e281aa9ed032f63d1784da5c345bc2a141a3b9
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-12-08 13:20:09 +0100

    feat: Allow to configure the namespace via environment variable STATUS_NAMESPACE

--- docker/ldap-server/tests/test_prepare_app.py
+++ docker/ldap-server/tests/test_prepare_app.py
@@ -1,4 +1,8 @@
+from typer.testing import CliRunner
 import pytest
+
+
+runner = CliRunner()
 
 
 def test_discovers_namespace_as_fallback(evaluate_database_init, mocker):
@@ -18,3 +22,16 @@ def test_uses_provided_namespace(evaluate_database_init, mocker):
     evaluate_database_init.prepare_app(namespace="stub_namespace")
     dn_mock.assert_not_called()
     assert evaluate_database_init.settings["namespace"] == "stub_namespace"
+
+
+def test_namespace_is_provided_via_environment(evaluate_database_init, mocker):
+    mocker.patch.object(evaluate_database_init, "configure_kubernetes_client")
+    evaluate_database_init.app.command()(stub_command)
+    mocker.patch.dict("os.environ", {"STATUS_NAMESPACE": "stub_namespace"})
+
+    runner.invoke(evaluate_database_init.app, ["stub-command"])
+    assert evaluate_database_init.settings["namespace"] == "stub_namespace"
+
+
+def stub_command():
+    pass
