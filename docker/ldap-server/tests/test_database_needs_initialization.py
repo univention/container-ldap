b549df46495334b8c9806a3ea4cfdbfbe1fa592c
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-12-08 18:03:59 +0100

    refactor: Sanitize logging of exceptions

--- docker/ldap-server/tests/test_database_needs_initialization.py
+++ docker/ldap-server/tests/test_database_needs_initialization.py
@@ -3,15 +3,13 @@ from unittest import mock
 import pytest
 
 
-def test_uses_configuration_from_settings(evaluate_database_init, stub_settings, client_mock, mocker):
+def test_uses_configuration_from_settings(evaluate_database_init, stub_settings, client_mock):
     stub_configmap = mock.Mock()
     stub_configmap.data = {"ldap_database_initialized": "false"}
     read_namespaced_config_map_mock = client_mock.CoreV1Api().read_namespaced_config_map
     read_namespaced_config_map_mock.return_value = stub_configmap
 
-    with pytest.raises(SystemExit) as exit_info:
-        evaluate_database_init.database_needs_initialization()
+    evaluate_database_init.database_needs_initialization()
 
-    assert exit_info.value.code == 0
     read_namespaced_config_map_mock.assert_called_once_with(
         name="stub_configmap", namespace="stub_namespace")
