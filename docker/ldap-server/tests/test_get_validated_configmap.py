19e1b3526478aa36381f9e54db68156057217d1d
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-12-08 18:26:58 +0100

    fix: Correct handling of empty ConfigMap

--- docker/ldap-server/tests/test_get_validated_configmap.py
+++ docker/ldap-server/tests/test_get_validated_configmap.py
@@ -11,3 +11,13 @@ def test_raises_value_error_when_key_is_missing(evaluate_database_init, stub_set
 
     with pytest.raises(ValueError):
         evaluate_database_init.get_validated_configmap()
+
+
+def test_handles_empty_configmap_correct(evaluate_database_init, stub_settings, client_mock):
+    stub_configmap = mock.Mock()
+    stub_configmap.data = None
+    read_namespaced_config_map_mock = client_mock.CoreV1Api().read_namespaced_config_map
+    read_namespaced_config_map_mock.return_value = stub_configmap
+
+    with pytest.raises(ValueError):
+        evaluate_database_init.get_validated_configmap()
