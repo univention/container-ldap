647864394b3d67cebe2e89b71680b0cf41f92353
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-12-08 17:44:53 +0100

    test: Move repeated code into utility fixtures

--- docker/ldap-server/tests/test_database_initialized.py
+++ docker/ldap-server/tests/test_database_initialized.py
@@ -3,15 +3,14 @@ from unittest import mock
 import pytest
 
 
-def test_uses_configuration_from_settings(evaluate_database_init, mocker):
+def test_uses_configuration_from_settings(evaluate_database_init, stub_settings, client_mock, mocker):
     client_mock = mocker.patch.object(evaluate_database_init, "client")
-    mocker.patch.dict(evaluate_database_init.settings, {
-        "configmap": "stub_configmap", "namespace": "stub_namespace",
-    })
+
     stub_configmap = mock.Mock()
     stub_configmap.data = {"ldap_database_initialized": "false"}
     read_namespaced_config_map_mock = client_mock.CoreV1Api().read_namespaced_config_map
     read_namespaced_config_map_mock.return_value = stub_configmap
+
     replace_namespaced_config_map_mock = client_mock.CoreV1Api().replace_namespaced_config_map
 
     evaluate_database_init.database_initialized()
