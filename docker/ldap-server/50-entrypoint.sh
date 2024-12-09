3b6ab9c1d2c81d2eda0cfe168706a3a17b4306a8
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-12-09 16:26:17 +0100

    feat: Only load initial content if "LDAP_SERVER_ROLE" is "primary" or unset

--- docker/ldap-server/50-entrypoint.sh
+++ docker/ldap-server/50-entrypoint.sh
@@ -279,7 +279,9 @@ setup_slapd_conf
 fetch_saml_metadata
 setup_sasl_mech_whitelist
 setup_sasl_mech_saml
-setup_initial_ldif
+if [ "${LDAP_SERVER_ROLE:-primary}" = "primary" ]; then
+  setup_initial_ldif
+fi
 setup_translog_ldif
 setup_tls
 # log_configuration
