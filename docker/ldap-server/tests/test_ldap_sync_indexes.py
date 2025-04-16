068c158851bcc65d776bb0538ebbaa1b27388372
Andreas Kosubek <andreas.kosubek@googlemail.com>
2025-04-16 15:43:06 +0200

    fix: UnitTests and refactoring
    univention/dev/internal/team-nubus#1019

--- docker/ldap-server/tests/test_ldap_sync_indexes.py
+++ docker/ldap-server/tests/test_ldap_sync_indexes.py
@@ -1,8 +1,11 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
-
+import logging
 import os
+from json import JSONDecodeError
 from pathlib import Path
+
+import pytest
 
 
 def test_parse_attributes(sync_ldap_indexes, schema_file):
@@ -48,10 +51,45 @@ def test_get_changed_attributes(sync_ldap_indexes, schema_file):
     indexes = sync_ldap_indexes.get_current_indexes()
     state = sync_ldap_indexes.get_state(indexes, attributes)
 
-    # statefile_indexes = ["univentionObjectIdentifier"]
     file_path = os.path.abspath(os.path.dirname(__file__))
     statefile_state = sync_ldap_indexes.read_state_file(Path(f"{file_path}/ldap-testfiles/test-ldap-statefile.json"))
 
     changed_attributes = sync_ldap_indexes.get_changed_attributes(statefile_state, state)
 
     assert expected_attributes == changed_attributes
+
+
+def test_broken_state_file(sync_ldap_indexes):
+    file_path = os.path.abspath(os.path.dirname(__file__))
+    with pytest.raises(JSONDecodeError):
+        sync_ldap_indexes.read_state_file(Path(f"{file_path}/ldap-testfiles/test-ldap-statefile-broken.json"))
+
+
+def test_main_function_broken_state_file(sync_ldap_indexes, caplog):
+    file_path = os.path.abspath(os.path.dirname(__file__))
+    schema_dirs = [Path(f"{file_path}/ldap-testfiles/")]
+    state_file_path = Path(f"{file_path}/ldap-testfiles/test-ldap-statefile-broken.json")
+    state_file_template_path = Path(f"{file_path}/ldap-testfiles/test-ldap-statefile.json")
+    mdb_file_path = Path(f"{file_path}/ldap-testfiles/data.mdb")
+
+    sync_ldap_indexes.main(schema_dirs, state_file_path, state_file_template_path, mdb_file_path)
+
+    assert len(caplog.records) == 1
+
+    assert caplog.records[0].levelno == logging.ERROR
+    assert "Error reading state file" in caplog.records[0].message
+
+
+def test_main_function(sync_ldap_indexes, caplog):
+    file_path = os.path.abspath(os.path.dirname(__file__))
+    schema_dirs = [Path(f"{file_path}/ldap-testfiles/")]
+    state_file_path = Path(f"{file_path}/ldap-testfiles/test-ldap-statefile-tmp.json")
+    state_file_template_path = Path(f"{file_path}/ldap-testfiles/test-ldap-statefile.json")
+    mdb_file_path = Path(f"{file_path}/ldap-testfiles/data.mdb")
+
+    sync_ldap_indexes.main(schema_dirs, state_file_path, state_file_template_path, mdb_file_path)
+
+    # assert len(caplog.records) == 1
+    #
+    # assert caplog.records[0].levelno == logging.ERROR
+    # assert "Error reading state file" in caplog.records[0].message
