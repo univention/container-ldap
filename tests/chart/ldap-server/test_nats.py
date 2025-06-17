2366f02f35901e4784abab02376db3c86f362f95
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-17 08:22:52 +0200

    feat(ldap-server): Render the Nats secret only when ldif-producer is enabled
    Otherwise the configuration of Nats access credentials would always be required.
    
    Issue: univention/dev/internal/team-nubus#892

--- tests/chart/ldap-server/test_nats.py
+++ tests/chart/ldap-server/test_nats.py
@@ -1,3 +1,8 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+from contextlib import nullcontext as does_not_raise
+
 from univention.testing.helm.client.nats import (
     Auth,
     Connection,
@@ -20,9 +25,23 @@ class TestAuth(SecretUsageViaEnv, Auth):
         "ldifProducer.nats": "nats",
     }
 
+    def test_auth_config_is_not_required_when_disabled(self, chart):
+        values = self.load_and_map(
+            """
+            ldifProducer:
+              enabled: false
+
+              nats:
+                auth:
+                  username: null
+                  password: null
+            """
+        )
+        with does_not_raise():
+            chart.helm_template(values)
+
 
 class TestConnection(ConnectionViaConfigMap, Connection):
-
     config_map_name = "release-name-ldap-server-ldif-producer-config"
 
     prefix_mapping = {
